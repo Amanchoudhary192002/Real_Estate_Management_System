5a7b01d10eb304cb8dd332d95a99d36d84bea68e
Aman choudhary <81174648+Amanchoudhary192002@users.noreply.github.com>
2022-07-05 14:34:41 +0530

    Delete Realestatesystem-RSMS-/Real_Estate_Management_System/repse directory

--- Realestatesystem-RSMS-/Real_Estate_Management_System/repse/nav_footer/adminfooter.php
+++
@@ -1,9 +0,0 @@
-<footer>
-<div class="bg-light py-4">
-      <div class="container text-center">
-      <center> 
-      <div class="credit">created by <span> Aman choudhary </span> | all rights reserved! </div>
-      </center> 
-      </div>
-    </div>
-</footer>
